055c9326628382432681491414d203d3a9020800
root <root@localhost.localdomain>
2019-12-09 21:38:38 +0100

    maj

--- bf-tp.py
+++ bf-tp.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 import hashlib
 import time
+import threading
 
 # you will see the 'bf' name use its the alias for brute force.
 
@@ -17,16 +18,15 @@ special = ['#','@','_']
 bfList = []
 typeCharacter = 0
 nbCharacters = [6, 13]
-pathFile = "./hash.txt"
+defaultPathFile = "./hash.txt" # dont change it
 
-            
+ 
 # method used to read the hashes password file
 def readFile(path):
     myFile = open(path, 'r+')
     result = []
-    with myFile as f:
-        for line in f.readlines():
-            result.append(line.rstrip())
+    for line in myFile.readlines():
+        result.append(line.rstrip())
     myFile.close()    
     return result
 
@@ -39,7 +39,6 @@ def encryptString(hash_string):
 # compare generated password to attempt hashed
 def checkPass(attempt, myHash):
     passToCheck = "".join(attempt)
-    print(passToCheck)
     hashToCheck = encryptString(passToCheck)
     if hashToCheck == myHash:
         return passToCheck
@@ -47,39 +46,15 @@ def checkPass(attempt, myHash):
 
 # method for generate hash and compare it
 def crackersOneList(cList, nbCharacter, pathHash):
+    input()
     doc = readFile(pathHash)
+    result = []
     for d in doc:
         start = time.time()
-        for attempt in itertools.product(cList, repeat=nbCharacter):
-            solution = checkPass(attempt, d)
-            if solution != False:
-                realTime = time.time()-start
-                print ('Le mot de passe est "' + solution + '" .')
-                minutes, seconds = divmod(int(realTime), 60)
-                hours, minutes = divmod(minutes, 60)
-                days, hours = divmod(hours, 24)
-                years, days = divmod(days, 365)
-                print ('Temps pour le trouver : {:d} ans, {:02d} jours, {:02d} heures, {:02d} minutes et {:02d} secondes'.format(years, days, hours, minutes, seconds))                 
-                break
-
-
-def timeToCrackTheoric(nbCharacters, typeCharacter):
-    for i in range(nbCharacters[0], nbCharacters[1]):
-        seconds = (i**typeCharacter)/600000
-        minutes, seconds = divmod(seconds, 60)
-        hours, minutes = divmod(minutes, 60)
-        days, hours = divmod(hours, 24)
-        years, days = divmod(days, 365)
-        print('Temps estime maximal pour trouver le mot de passe de {:d} caracteres : {:d} ans, {:02d} jours, {:02d} heures, {:02d} minutes et {:02d} secondes '.format(i, years, days, hours, minutes, seconds))
-
-def testTimeToCrackTheoric(cList, nbCharacters, pathHash):
-    doc = readFile(pathHash)
-    for d in doc:
-        start = time.time()
+        solution = ""
         i = nbCharacters[0]
         while i < nbCharacters[1]: 
             for attempt in itertools.product(cList, repeat=i):
-                print(attempt)
                 solution = checkPass(attempt, d)
                 if solution != False:
                     realTime = time.time()-start
@@ -89,8 +64,58 @@ def testTimeToCrackTheoric(cList, nbCharacters, pathHash):
                     days, hours = divmod(hours, 24)
                     years, days = divmod(days, 365)
                     print ('Temps pour le trouver : {:d} ans, {:02d} jours, {:02d} heures, {:02d} minutes et {:02d} secondes'.format(years, days, hours, minutes, seconds))                 
+                    result.append(attempt)
+                    i = nbCharacters[1]
                     break
-                i += 1
+            i +=1
+    print(result)
+
+# test time to crack
+def testTimeToCrackTheoric(cList, nbCharacter, defaultPathFile):
+    timeSpent = []
+    stop = 1000000
+    xtime = time.time()
+    doc = readFile(defaultPathFile)
+    timeSpent.append(time.time()-xtime)
+    result = []
+    for d in doc:
+        start = time.time()
+        solution = ""
+        xtime = time.time() 
+        for attempt in itertools.product(cList, repeat=6):
+            solution = checkPass(attempt, d)
+            if solution != False:
+                realTime = time.time()-start
+                print ('Le mot de passe est "' + solution + '" .')
+                minutes, seconds = divmod(int(realTime), 60)
+                hours, minutes = divmod(minutes, 60)
+                days, hours = divmod(hours, 24)
+                years, days = divmod(days, 365)
+                print ('Temps pour le trouver : {:d} ans, {:02d} jours, {:02d} heures, {:02d} minutes et {:02d} secondes'.format(years, days, hours, minutes, seconds))                 
+                result.append(attempt)
+                i = nbCharacter[1]
+                break
+            if stop != 0:
+                stop = stop-1
+            else:
+                break
+        timeSpent.append(time.time()-xtime)
+    realTimeSpent = timeSpent[0] + timeSpent[1]
+    return realTimeSpent
+ 
+def timeToCrackTheoric(nbCharacters, typeCharacter, cList, defaultPathFile):
+    showTime = []
+    print(typeCharacter)
+    for i in range(nbCharacters[0], nbCharacters[1]):
+        result = testTimeToCrackTheoric(cList, i, defaultPathFile)
+        seconds = (typeCharacter**i)*(result/1000000)        
+        minutes, seconds = divmod(int(seconds), 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+        years, days = divmod(days, 365)
+        showTime.append('Temps estime maximal pour trouver le mot de passe de {:d} caracteres : {:d} ans, {:02d} jours, {:02d} heures, {:02d} minutes et {:02d} secondes '.format(i, years, days, hours, minutes, seconds))
+    for show in showTime:
+        print(show)
 
 # use the command parser
 parser = argparse.ArgumentParser(description='CrackPass : command line brute force')
@@ -131,7 +156,9 @@ for index, value in enumerate(sys.argv):
             pathFile = sys.argv[index+1]
     # Launch testing bruteforce script
     elif value == '-T' or value == '--test':
-        testTimeToCrackTheoric(bfList, nbCharacters, pathFile)
+        testTimeToCrackTheoric(bfList, nbCharacters, defaultPathFile)
             
-# timeToCrackTheoric(nbCharacters, typeCharacter)
-# crackersOneList(bfList, nbCharacter, pathsFile)
+timeToCrackTheoric(nbCharacters, typeCharacter, bfList, defaultPathFile)
+crackersOneList(bfList, nbCharacter, pathFile)
+
+# threading.Thread(crackersOneList, bfList, nbCharacter, pathFile).start()
